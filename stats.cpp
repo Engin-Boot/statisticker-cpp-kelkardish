792cace9e1a96a2fd7ea5c56b747e4b8c4844abe
kelkardish <69232304+kelkardish@users.noreply.github.com>
2020-09-02 09:03:14 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,49 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+double computeAverage(const std::vector<double>& passedStats, int n) {
+	
+	double sum = 0.0;
+	for (int i = 0; i < n; i++) {
+		sum = sum + passedStats[i];
+	}
+
+	return sum / n;
+}
+
+double computeMax(const std::vector<double>& passedStats, int n) {
+	
+	double max = passedStats[0];
+	for (int i = 1; i < n; i++) {
+		if (max < passedStats[i])
+			max = passedStats[i];
+ 	}
+	return max;
+}
+
+double computeMin(const std::vector<double>& passedStats, int n) {
+	double min = passedStats[0];
+	for (int i = 1; i < n; i++) {
+		if (min > passedStats[i])
+			min = passedStats[i];
+	}
+	return min;
+}
+
+Stats Statistics::ComputeStatistics(const std::vector<double>& passedStats) {
+
+	Stats calculatedStats = { NAN, NAN, NAN };
+
+	int inputSize = passedStats.size();
+
+	if (inputSize == 0) {
+		return calculatedStats;
+	}
+	else {
+		calculatedStats.average = computeAverage(passedStats, inputSize);
+		calculatedStats.max = computeMax(passedStats, inputSize);
+		calculatedStats.min = computeMin(passedStats, inputSize);
+	}
+
+	return calculatedStats;
+
 }
