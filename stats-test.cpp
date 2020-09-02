a157df1a7c8efc90d1729aa90b48d6777103d57e
kelkardish <69232304+kelkardish@users.noreply.github.com>
2020-09-02 09:30:46 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,9 +1,10 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
+#include <cmath>
 #include "stats.h"
 
-#include <cmath>
+
 
 TEST_CASE("reports average, minimum and maximum") {
     auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
