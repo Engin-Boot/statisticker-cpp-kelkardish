7c1666d8e459600f5b2782eede5ca7e54478fe8e
kelkardish <69232304+kelkardish@users.noreply.github.com>
2020-09-04 09:28:16 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,49 +1,70 @@
 #include "stats.h"
-#include <cmath>
-double computeAverage(const std::vector<double>& passedStats, int n) {
-	
-	double sum = 0.0;
-	for (int i = 0; i < n; i++) {
-		sum = sum + passedStats[i];
-	}
 
-	return sum / n;
+typedef std::vector<double> vecDouble;
+
+class StatClass
+{
+private:
+	vecDouble stats;
+	int statSize;
+	void stripNANvalues();
+public:
+	StatClass(const vecDouble& x) : stats(x) { stripNANvalues(); }
+	double computeAverage();
+	double computeMax();
+	double computeMin();
+};
+
+
+void StatClass::stripNANvalues() {
+	statSize = stats.size();
+	for (int i = 0; i < statSize; i++) {
+		if (std::isnan(stats[i])) {
+			stats[i] = 0.0;
+		}
+	}
 }
 
-double computeMax(const std::vector<double>& passedStats, int n) {
-	
-	double max = passedStats[0];
-	for (int i = 1; i < n; i++) {
-		if (max < passedStats[i])
-			max = passedStats[i];
- 	}
+double StatClass::computeAverage() {
+	double sum = 0.0;
+	for (int i = 0; i < statSize; i++) {
+		sum = sum + stats[i];
+	}
+	return sum / statSize ;
+}
+
+double StatClass::computeMax() {
+	double max = stats[0];
+	for (int i = 1; i < statSize; i++) {
+		if (max < stats[i]) {
+			max = stats[i];
+		}
+	}
 	return max;
 }
 
-double computeMin(const std::vector<double>& passedStats, int n) {
-	double min = passedStats[0];
-	for (int i = 1; i < n; i++) {
-		if (min > passedStats[i])
-			min = passedStats[i];
+double StatClass::computeMin() {
+	double min = stats[0];
+	for (int i = 1; i < statSize; i++) {
+		if (min > stats[i]) {
+			min = stats[i];
+		}
 	}
 	return min;
 }
 
-Stats Statistics::ComputeStatistics(const std::vector<double>& passedStats) {
+Stats Statistics::ComputeStatistics(const vecDouble& passedStats) {
+	Stats computedStats = {NAN, NAN, NAN};
 
-	Stats calculatedStats = { NAN, NAN, NAN };
-
-	int inputSize = passedStats.size();
-
-	if (inputSize == 0) {
-		return calculatedStats;
-	}
-	else {
-		calculatedStats.average = computeAverage(passedStats, inputSize);
-		calculatedStats.max = computeMax(passedStats, inputSize);
-		calculatedStats.min = computeMin(passedStats, inputSize);
+	if (passedStats.size() == 0) {
+		return computedStats;
 	}
 
-	return calculatedStats;
+	StatClass* statobj = new StatClass(passedStats);
+	
+	computedStats.average = statobj->computeAverage();
+	computedStats.max = statobj->computeMax();
+	computedStats.min = statobj->computeMin();
 
+	return computedStats;
 }
